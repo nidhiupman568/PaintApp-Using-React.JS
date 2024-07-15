0051e4fa3af32b563e57d1c51858554562f06a87
nidhiupman568 <upmanyunidhi3092@gmail.com>
2024-07-15 23:13:28 +0530

    do something creative

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useEffect, useRef, useState } from "react"; 
+import Menu from "./Components/Menu"; 
+import "./App.css"; 
+
+function App() { 
+	const canvasRef = useRef(null); 
+	const ctxRef = useRef(null); 
+	const [isDrawing, setIsDrawing] = useState(false); 
+	const [lineWidth, setLineWidth] = useState(5); 
+	const [lineColor, setLineColor] = useState("black"); 
+	const [lineOpacity, setLineOpacity] = useState(0.1); 
+
+	// Initialization when the component 
+	// mounts for the first time 
+	useEffect(() => { 
+		const canvas = canvasRef.current; 
+		const ctx = canvas.getContext("2d"); 
+		ctx.lineCap = "round"; 
+		ctx.lineJoin = "round"; 
+		ctx.globalAlpha = lineOpacity; 
+		ctx.strokeStyle = lineColor; 
+		ctx.lineWidth = lineWidth; 
+		ctxRef.current = ctx; 
+	}, [lineColor, lineOpacity, lineWidth]); 
+
+	// Function for starting the drawing 
+	const startDrawing = (e) => { 
+		ctxRef.current.beginPath(); 
+		ctxRef.current.moveTo( 
+			e.nativeEvent.offsetX, 
+			e.nativeEvent.offsetY 
+		); 
+		setIsDrawing(true); 
+	}; 
+
+	// Function for ending the drawing 
+	const endDrawing = () => { 
+		ctxRef.current.closePath(); 
+		setIsDrawing(false); 
+	}; 
+
+	const draw = (e) => { 
+		if (!isDrawing) { 
+			return; 
+		} 
+		ctxRef.current.lineTo( 
+			e.nativeEvent.offsetX, 
+			e.nativeEvent.offsetY 
+		); 
+
+		ctxRef.current.stroke(); 
+	}; 
+
+	return ( 
+		<div className="App"> 
+			<h1>Paint App</h1> 
+			<div className="draw-area"> 
+				<Menu 
+					setLineColor={setLineColor} 
+					setLineWidth={setLineWidth} 
+					setLineOpacity={setLineOpacity} 
+				/> 
+				<canvas 
+					onMouseDown={startDrawing} 
+					onMouseUp={endDrawing} 
+					onMouseMove={draw} 
+					ref={canvasRef} 
+					width={`1280px`} 
+					height={`720px`} 
+				/> 
+			</div> 
+		</div> 
+	); 
+} 
 
 export default App;
